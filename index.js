10748d8dfe679f31d08e320507b18c9f5b46defa
OssianN <ossian.northen@gmail.com>
2022-01-30 16:14:36 +0100

    adds initial socket setup

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 
 const port = 4000;
 
@@ -14,7 +18,6 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
-
 
 app.listen(process.env.PORT || port, () => console.log(`server running on port ${process.env.PORT || port}`));
 
