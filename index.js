5317d9f7f00b577bd8ff0106f6b5a6773a24ffee
OssianN <ossian.northen@gmail.com>
2022-01-30 15:35:11 +0100

    adds initial socket setup

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+const server = require('http').Server(app);
+
+const socketIo = require("socket.io");
+const io = socketIo(server)
+
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
+
 
 app.listen(process.env.PORT || port, () => console.log(`server running on port ${process.env.PORT || port}`));
 
