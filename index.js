720599967937a59a69b38770da542916e46fb5b2
OssianN <ossian.northen@gmail.com>
2022-01-30 17:13:37 +0100

    tests cors

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const app = require('express')()
 const http = require('http').createServer(app)
-const io = require('socket.io')(http,  {
-  cors: {
-    origin: "http://localhost:3000"
-  }
-})
-
-app.get('/', (req, res) => {
-  res.json("hello world!");
-});
+const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
   console.log('a user connected');
